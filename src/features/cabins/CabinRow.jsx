276aacdc2b3d1fcefbdf45d6f387759c853221c2
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2025-01-17 11:03:24 +0600

    filtering client side cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,23 +1,13 @@
 import styled from "styled-components";
-import {formatCurrency} from "../../utils/helpers.js";
+import { formatCurrency } from "../../utils/helpers.js";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import { useDeleteCabin } from "./useDeleteCabin.js";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from "../../ui/Table.jsx";
+import Menus from "../../ui/Menus.jsx";
 
 const Img = styled.img`
   display: block;
@@ -46,68 +36,71 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 
-const CabinRow = ({cabin}) => {
+const CabinRow = ({ cabin }) => {
+  const { isDeleting, deleteCabin } = useDeleteCabin();
+  const { isCreating, createCabin } = useCreateCabin();
 
-  
-  const {isDeleting, deleteCabin} = useDeleteCabin();
-  const {isCreating, createCabin} = useCreateCabin();
-  const {id:cabinId, name, maxCapacity, regularPrice, discount, image, description} = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    description,
+  } = cabin;
 
-  function handleDuplicate(){
+  function handleDuplicate() {
     createCabin({
-      name : `copy of ${name}`,
-      maxCapacity, regularPrice , discount , image , description,
+      name: `Copy of ${name}`,
+      maxCapacity,
+      regularPrice,
+      discount,
+      image,
+      description,
     });
   }
 
-  
-
-// ei code ta pore cabins er useDeleteCabin.js file e rakha hoiche.
-
-//  const queryClient = useQueryClient();
- 
-//   const {isLoading: isDeleting , mutate} =  useMutation({
-//     mutationFn: deleteCabin,
-//     onSuccess: () => {
-//       toast.success("cabin successfully deleted")
-//       queryClient.invalidateQueries({
-//         queryKey: ["cabins"],
-//       });
-//     },
-//     onError:err => toast.error(err.message),
-  
-//   });
-
-
-
   return (
-    
-    <TableRow role="row">
-      <Img src={image} />
+    <Table.Row role="row">
+      <Img src={image} alt={`${name} image`} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
-      <Price>{formatCurrency(regularPrice)} </Price>
-      {discount ? (<Discount>{formatCurrency(discount)} </Discount>) : ( <span>&mdash;</span> )}
+      <Price>{formatCurrency(regularPrice)}</Price>
+      {discount ? (
+        <Discount>{formatCurrency(discount)}</Discount>
+      ) : (
+        <span>&mdash;</span>
+      )}
       <div>
-        <button disabled={isCreating} onClick={handleDuplicate}><HiSquare2Stack/></button>
         <Modal>
-        <Modal.Open opens="edit">
-        <button><HiPencil/></button>
-        </Modal.Open>
-        <Modal.Window name="edit">
-          <CreateCabinForm cabinToEdit={cabin}/>
-        </Modal.Window>
-        <Modal.Open>
-        <button><HiTrash/></button>
-        </Modal.Open>
-        <Modal.Window>
-          <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={()=> deleteCabin(cabinId)}/>
-        </Modal.Window>
+          <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+            <Menus.List id={cabinId}>
+              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+                Duplicate
+              </Menus.Button>
+              <Modal.Open opens="edit">
+                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              </Modal.Open>
+              <Modal.Open opens="delete">
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+              </Modal.Open>
+            </Menus.List>
+            <Modal.Window name="edit">
+              <CreateCabinForm cabinToEdit={cabin} />
+            </Modal.Window>
+            <Modal.Window name="delete">
+              <ConfirmDelete
+                resourceName="cabins"
+                disabled={isDeleting}
+                onConfirm={() => deleteCabin(cabinId)}
+              />
+            </Modal.Window>
+          </Menus.Menu>
         </Modal>
       </div>
-    </TableRow>
-    
-    
+    </Table.Row>
   );
 };
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,6 +2,7 @@
 
 import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
@@ -13,7 +14,7 @@ function Cabins() {
     <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>Filter / Sort</p>
+      <CabinTableOperations/>
     </Row>
     <Row>
       <CabinTable/>
